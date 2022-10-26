45a89638b70f716ae337daae9ca5c78d484cde5d
Nikita <nikitasch2001@gmail.com>
2022-10-26 21:56:40 +0200

    Homework

---
+++ src/LESSON/CLASSWORK/class2510.java
@@ -0,0 +1,81 @@
+/*package LESSON.CLASSWORK;
+
+public class class2510 {
+ /*   package LESSON.CLASSWORK;
+
+    public class class2610 {
+        //массив
+        public static void main(String[] args) {
+            boolean booleanValue = false;
+            //int value = 1, 2, 3, 4, 5;
+
+            // тип данных, [], название переменной = иницивлизация массива.
+            // type[] variable = new type[length]; 1 case length = max int value, length = 0
+            // type variable[] = new type[length];
+            //int value = 5;
+            int[] arrayInts = new int[5];
+
+            boolean[] booleanArrays = new boolean[3];
+            // stack head
+            // lifo
+
+            //element - cell => value type
+            // index - address element
+
+            //get value from array variable[index];
+            //System.out.println(arrayInts[0]);
+
+            //add or set value into array
+            //array variable [index] = value
+
+            arrayInts[0] = 101;
+            arrayInts[1] = 151;
+            arrayInts[2] = 351;
+            arrayInts[3] = 51;
+            arrayInts[4] = 201;
+
+            System.out.println(arrayInts[0]);
+            System.out.println(arrayInts[1]);
+            System.out.println(arrayInts[2]);
+            System.out.println(arrayInts[3]);
+            System.out.println(arrayInts[4]);
+
+
+            int[] filledValueArrays = {1, 2, 3, 4, 10, 11, 200, 234,};
+
+
+            System.out.println(filledValueArrays[5]);
+
+    /*    int[] superArray = new int[Integer.MAX_VALUE];
+        System.out.println("Array is created");
+
+     */
+
+
+
+  /*          int valueInt1 = 10;
+            int valueInt2 = 10;
+            System.out.println(equalsTwoIntegerNumber(ValueInt1, valueInt2));
+
+            int[] newArray1 = {1, 2, 3, 4};
+            int[] newArray2 = {1, 2, 3, 4};
+
+            System.out.println(equals(newArray1,newArray2));
+
+
+        }
+
+        public static boolean equalsTwoIntegerNumber(int number1, int number2) {
+            return number1 == number2;
+        }
+
+        public static boolean equals(int[] array1, int[] array2) {
+            return array1 ==array2;
+        }
+
+
+
+    }
+
+}
+*/
